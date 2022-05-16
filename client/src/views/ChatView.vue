8897c7a0b1b798f331665d2b336c9d4dcbfef1fd
Eduardo Fernández <feguedi@yahoo.com.mx>
2022-05-15 22:41:29 -0500

    Vista del chat - Lectura de variables de entorno del cliente

---
+++ client/src/views/ChatView.vue
@@ -0,0 +1,12 @@
+<template>
+  <h1 class="text-3xl font-bold underline">Chat app</h1>
+</template>
+
+<script>
+/* eslint-env node */
+export default {
+  mounted() {
+    console.log("Variables de entorno:", import.meta.env.VITE_SERVERURL);
+  },
+};
+</script>
--- client/vite.config.js
+++ client/vite.config.js
@@ -1,14 +1,18 @@
+/* eslint-env node */
 import { fileURLToPath, URL } from "url";
 
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig((command, mode) => ({
   plugins: [vue()],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
-});
+  define: {
+    __APP_ENV__: loadEnv(mode, process.cwd(), "").VUE_APP_SERVERURL,
+  },
+}));
